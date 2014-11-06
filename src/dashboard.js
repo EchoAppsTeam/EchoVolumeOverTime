785684a6a23d8f8fc71b9463fb7fdc0bfeab4ebb
Andrew Kushnir <kushnir.andrew@gmail.com>
2014-11-05 18:41:06 -0800

    v1.0.7 - Moving Data Source config to the bottom of the properties grid

--- src/dashboard.js
+++ src/dashboard.js
@@ -26,20 +26,6 @@ dashboard.dependencies = [{
 }];
 
 dashboard.config.ecl = [{
-	"name": "targetURL",
-	"component": "Echo.DataServer.Controls.Dashboard.DataSourceGroup",
-	"type": "string",
-	"required": true,
-	"config": {
-		"title": "",
-		"labels": {
-			"dataserverBundleName": "Echo Historical Volume Auto-Generated Bundle for {instanceName}"
-		},
-		"apiBaseURLs": {
-			"DataServer": "{%= apiBaseURLs.DataServer %}/"
-		}
-	}
-}, {
 	"component": "Group",
 	"name": "presentation",
 	"type": "object",
@@ -144,6 +130,21 @@ dashboard.config.ecl = [{
 			"options": []
 		}
 	}]
+}, {
+	"name": "targetURL",
+	"component": "Echo.DataServer.Controls.Dashboard.DataSourceGroup",
+	"type": "string",
+	"required": true,
+	"config": {
+		"title": "",
+		"expanded": false,
+		"labels": {
+			"dataserverBundleName": "Echo Historical Volume Auto-Generated Bundle for {instanceName}"
+		},
+		"apiBaseURLs": {
+			"DataServer": "{%= apiBaseURLs.DataServer %}/"
+		}
+	}
 }];
 
 dashboard.modifiers = {
